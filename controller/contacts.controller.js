ef891cbe2eb723ab47413760467319b47682c7aa
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-03-25 21:16:15 +0530

    some chnage

--- controller/contacts.controller.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-// import Contacts from "../Models/contacts.models.js";
-
-export const getContact = async (req, res) => {
-  // const contact = await Contacts.find();
-  // res.json(contact);
-
-  res.render("home");
-};
