c49a630a370c544bd3d546e87c882ab020646037
ZEESHAN8692 <162082741+ZEESHAN8692@users.noreply.github.com>
2025-03-26 23:22:10 +0530

    90% Complete

--- controller/contact.controller.js
+++ controller/contact.controller.js
@@ -7,17 +7,28 @@ export const getContact = async (req, res) => {
 };
 
 export const showContactPage = async (req, res) => {
-  res.render("show-contact");
+  const contact = await Contact.findById(req.params.id);
+  res.render("show-contact", { contact });
 };
 export const addContactPage = async (req, res) => {
   res.render("add-contacts");
 };
 
-export const addContact = async (req, res) => {};
-
-export const updateContactPage = async (req, res) => {
-  res.render("update-contact");
+export const addContact = async (req, res) => {
+  const contact = await Contact.create(req.body);
+  res.redirect("/");
 };
 
-export const updateContact = async (req, res) => {};
-export const deleteContact = async (req, res) => {};
+export const updateContactPage = async (req, res) => {
+  const contact = await Contact.findById(req.params.id);
+  res.render("update-contact", { contact });
+};
+
+export const updateContact = async (req, res) => {
+  const contact = await Contact.findByIdAndUpdate(req.params.id, req.body);
+  res.redirect("/");
+};
+export const deleteContact = async (req, res) => {
+  await Contact.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+};
--- routes/countact.routes.js
+++ routes/countact.routes.js
@@ -12,8 +12,8 @@ const router = express.Router();
 
 router.get("/", getContact);
 router.get("/show-contact/:id", showContactPage);
-router.get("/add-contacts/:id", addContactPage);
-router.post("/add-contact/:id", addContact);
+router.get("/add-contacts", addContactPage);
+router.post("/add-contacts/", addContact);
 router.get("/update-contact/:id", updateContactPage);
 router.post("/update-contact/:id", updateContact);
 router.get("/delete-contact/:id", deleteContact);
